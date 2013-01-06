fcd7ba50187ec82e2d8cc5460d07e47fe59de72a
Jared Meeker <jared@meekers.org>
2013-01-06 16:30:10 -0700

    Mobs are now repelled and do not target. Each mob type has its own value in the configuration file.

--- src/org/meekers/plugins/aura/AuraPluginListener.java
+++ src/org/meekers/plugins/aura/AuraPluginListener.java
@@ -4,11 +4,14 @@
  */
 package org.meekers.plugins.aura;
 
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityTargetEvent;
+import org.bukkit.util.Vector;
 
 /**
  *
@@ -23,10 +26,56 @@ class AuraPluginListener implements Listener {
     }
 
     @EventHandler
-    public void onEntityHit(EntityTargetEvent event) {
-        if (event.getTarget().getType() == EntityType.PLAYER) {
+    public void onEntityTarget(EntityTargetEvent event) {
+        boolean repel = false;
+        Entity e = event.getEntity();
+        Entity t = event.getTarget();
+        
+        // If an entity dies, e may be null
+        if (e == null) {
+            return;
+        }
+        if (t.getType() == EntityType.PLAYER && e instanceof LivingEntity) {
             Player p = (Player) event.getTarget();
-            if (p.getLevel() >= plugin.getConfig().getInt("minxp")) {
+            int playerxp = p.getLevel();
+            double speed = plugin.getConfig().getDouble("repelspeed");
+            int zoxp = plugin.getConfig().getInt("zoxp");
+            int spxp = plugin.getConfig().getInt("spxp");
+            int skxp = plugin.getConfig().getInt("skxp");
+            int crxp = plugin.getConfig().getInt("crxp");
+            int enxp = plugin.getConfig().getInt("enxp");
+
+            // Zombie
+            if (playerxp >= zoxp && e.getType() == EntityType.ZOMBIE) {
+                repel = true;
+            }
+            
+            // Spider
+            if (playerxp >= spxp && e.getType() == EntityType.SPIDER) {
+                repel = true;
+            }
+            
+            // Skeleton
+            if (playerxp >= skxp && e.getType() == EntityType.SKELETON) {
+                repel = true;
+            }
+
+            // Creeper
+            if (playerxp >= crxp && e.getType() == EntityType.CREEPER) {
+                repel = true;
+            }
+            
+            // Enderman
+            if (playerxp >= enxp && e.getType() == EntityType.ENDERMAN) {
+                repel = true;
+            }
+            
+            if (repel == true) {
+                // Get velocity unit vector:
+                Vector unitVector = e.getLocation().toVector().subtract(p.getLocation().toVector()).normalize();
+                // Set speed and push entity:
+                e.setVelocity(unitVector.multiply(speed));
+                //p.sendMessage("Your XP just repelled a " + e.getType().getName());
                 event.setCancelled(true);
             }
         }
